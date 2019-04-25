b307a6684b44e2105209eb6469a7e96d771d233b
godyn <godyn.ola@gmail.com>
2019-04-25 18:17:04 +0200

    Task 19.3 - Functional implementation - delete method

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -17,7 +18,15 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(Long id){
+    public Optional<Task> getTaskById(final Long id){
         return repository.findById(id);
+    }
+
+    public Task saveTask(Task task){
+        return repository.save(task);
+    }
+
+    public void deleteTask(Long id){
+        repository.deleteById(id);
     }
 }
