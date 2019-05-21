c0caf5db41a68dc4d6d8e4ac6d4edfb2468dc738
godyn <godyn.ola@gmail.com>
2019-05-21 21:12:51 +0200

    Task 23.3 - Mail Service

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,41 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail(){
+        //GIVEN
+        Mail mail = new Mail("test@test.com", "test", "Test message", null);
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(mail.getReceiverEmail());
+        simpleMailMessage.setSubject(mail.getSubject());
+        simpleMailMessage.setText(mail.getMessage());
+        simpleMailMessage.setCc(mail.getToCc());
+
+        //WHEN
+        simpleEmailService.send(mail);
+
+        //THEN
+        verify(javaMailSender, times(1)).send(simpleMailMessage);
+    }
+}
